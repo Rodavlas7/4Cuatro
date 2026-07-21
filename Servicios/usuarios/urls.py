2078609097ffbaf45d721850ab1dae3f52b936be
Marlene <yeseniagtz06@gmail.com>
2026-07-20 20:43:58 -0700

    actualizacion

--- Servicios/usuarios/urls.py
+++ Servicios/usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import LoginAPIView
+from usuarios import views
 
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
-    
+    path("Empleado/Registrar/", views.RegistroEmpleadoAPIView.as_view(), name="registro_empleado")
 ]
